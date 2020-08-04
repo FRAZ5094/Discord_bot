77e446cbd6078e0766119ffcd951fb75888be107
FRAZ5094 <frasergmorrison@btinternet.com>
2020-08-04 12:23:53 +0100

    prettier

--- Bot.py
+++ Bot.py
@@ -1,8 +1,8 @@
 import discord
-from discord.ext import commands,tasks
+from discord.ext import commands, tasks
 from secrets import discord_bot_token
 from twitch_notifications import *
-import os 
+import os
 from datetime import datetime
 
 
@@ -15,127 +15,143 @@ async def authorized(ctx):
     else:
         return False
 
+
 def get_settings():
-    with open("settings.json","r") as f:
+    with open("settings.json", "r") as f:
         return json.load(f)
 
+
 def get_setting_value(setting):
-    settings=get_settings()
+    settings = get_settings()
     return settings[setting]
+
 
 @client.event
 async def on_ready():
     streamer_live_check.start()
-    await client.change_presence(status=discord.Status.online,activity=discord.Game(get_setting_value("status")))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(get_setting_value("status")))
     print("Bot online")
+
 
 @client.command()
 async def hello(ctx):
     await ctx.send("hello")
 
+
 @client.command()
-async def clear(ctx,amount):
-    if amount=="all":
+async def clear(ctx, amount):
+    if amount == "all":
         await ctx.channel.purge(limit=999999)
     await ctx.channel.purge(limit=int(amount)+1)
 
+
 @clear.error
-async def clear_error(ctx,error):
-    if isinstance(error,commands.MissingRequiredArgument):
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("Please specify and amount of messages to delete")
 
+
 @client.event
-async def on_command_error(ctx,error):
-    if isinstance(error,commands.CommandNotFound):
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
         await ctx.send("Command doesn't exist")
-    if isinstance(error,commands.CheckFailure):
+    if isinstance(error, commands.CheckFailure):
         await ctx.send("you dont have permission for this command")
+
 
 @client.command()
 async def ping(ctx):
     await ctx.send("{}ms".format(round(client.latency*1000)))
 
+
 @tasks.loop(minutes=get_setting_value("refresh-time"))
 async def streamer_live_check():
-    now=datetime.now()
-    current_time=now.strftime("%H:%M")
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
     print("Live check at:", current_time)
-    subs=read_json()
-    streamers_to_check=list(subs.keys())
-    if len(streamers_to_check)==0:
+    subs = read_json()
+    streamers_to_check = list(subs.keys())
+    if len(streamers_to_check) == 0:
         return
-    #get lists of offline/offline streamers
-    online_streamer_data=get_streams(streamers_to_check)
-    online_list,offline_list=streamer_lists(online_streamer_data)
+    # get lists of offline/offline streamers
+    online_streamer_data = get_streams(streamers_to_check)
+    online_list, offline_list = streamer_lists(online_streamer_data)
 
-    #increment the offline streamers offline-time 
-    increment_offline_time(offline_list,get_setting_value("refresh-time"))
+    # increment the offline streamers offline-time
+    increment_offline_time(offline_list, get_setting_value("refresh-time"))
 
-    #sort data into dict: {"message":[list to send message to]}
-    messages={}
+    # sort data into dict: {"message":[list to send message to]}
+    messages = {}
     for stream in online_streamer_data:
-            user_name=stream["user_name"]
-            messages[user_name]={}
-            title=stream["title"]
-            message=f"{user_name} is live!\nTitle: {title}\nLink: https://www.twitch.tv/{user_name}"
-            messages[user_name]={"message":message,"send_to":subs[user_name]["subs"]}
+        user_name = stream["user_name"]
+        messages[user_name] = {}
+        title = stream["title"]
+        message = f"{user_name} is live!\nTitle: {title}\nLink: https://www.twitch.tv/{user_name}"
+        messages[user_name] = {"message": message,
+                               "send_to": subs[user_name]["subs"]}
 
-    notifications=check_offline_time(online_list,messages,get_setting_value("maxoffline-time"))
+    notifications = check_offline_time(
+        online_list, messages, get_setting_value("maxoffline-time"))
 
     for message_info in notifications.values():
         for user_id in message_info["send_to"]:
-            await dm(user_id,message_info["message"])
+            await dm(user_id, message_info["message"])
+
 
 @client.command()
 async def check_online(ctx):
-    user_id=ctx.author.id
-    subbed_list=get_subbed_list(user_id)
-    if len(subbed_list)==0:
+    user_id = ctx.author.id
+    subbed_list = get_subbed_list(user_id)
+    if len(subbed_list) == 0:
         await ctx.send("Your not subbed to anyone")
-        return 
-    online_streamer_data=get_streams(subbed_list)
-    online_list,offline_list=streamer_lists(online_streamer_data)
-    message=""
+        return
+    online_streamer_data = get_streams(subbed_list)
+    online_list, offline_list = streamer_lists(online_streamer_data)
+    message = ""
     for streamer in online_list:
-        message+=f":white_check_mark: {streamer}--> <https://www.twitch.tv/{streamer}>\n"
-    subs=read_json()
+        message += f":white_check_mark: {streamer}--> <https://www.twitch.tv/{streamer}>\n"
+    subs = read_json()
     for streamer in offline_list:
-        offline_time=subs[streamer]["offline-time"]
-        if offline_time>=9999999999999:
-            time_phrase="unknown"
+        offline_time = subs[streamer]["offline-time"]
+        if offline_time >= 9999999999999:
+            time_phrase = "unknown"
         else:
-            mins=int(offline_time%60)
-            offline_time/=60
-            offline_time=int(offline_time)
-            time_phrase=time_formatting(offline_time,mins)
-        message+=f":x: {streamer}--> last live: {time_phrase}\n"
+            mins = int(offline_time % 60)
+            offline_time /= 60
+            offline_time = int(offline_time)
+            time_phrase = time_formatting(offline_time, mins)
+        message += f":x: {streamer}--> last live: {time_phrase}\n"
     await ctx.send(message)
 
+
 @client.command()
-async def channel(channel_id,message):
+async def channel(channel_id, message):
     channel = client.get_channel(int(channel_id))
     await channel.send(message)
 
-async def dm(user_id,message):
-        target= await client.fetch_user(user_id)
-        await target.send(message)
+
+async def dm(user_id, message):
+    target = await client.fetch_user(user_id)
+    await target.send(message)
+
 
 @client.command()
 async def sub_list(ctx):
-    user_id=ctx.author.id
-    subbed_list=get_subbed_list(user_id)
-    if len(subbed_list)==0:
+    user_id = ctx.author.id
+    subbed_list = get_subbed_list(user_id)
+    if len(subbed_list) == 0:
         await ctx.send("Your not subbed to anyone")
     else:
         await ctx.send("Your subscribed list:\n   "+"\n   ".join(subbed_list))
 
+
 @client.command()
-async def add_streamer(ctx,streamer):
-    user_id=ctx.author.id
-    subs=read_json()
-    all_streamers=list(subs.keys())
-    streamer=get_correct_user_name(streamer)
-    if streamer=="not a valid user name":
+async def add_streamer(ctx, streamer):
+    user_id = ctx.author.id
+    subs = read_json()
+    all_streamers = list(subs.keys())
+    streamer = get_correct_user_name(streamer)
+    if streamer == "not a valid user name":
         await ctx.send("not a valid user name, try again")
     else:
         if streamer in all_streamers:
@@ -147,17 +163,18 @@ async def add_streamer(ctx,streamer):
                 await ctx.send(f"Successfully subscribed to {streamer}")
                 write_to_json(subs)
                 await sub_list(ctx)
-                
+
         else:
-            subs[streamer]={"subs": [user_id],"offline-time": 9999999999999}
+            subs[streamer] = {"subs": [user_id], "offline-time": 9999999999999}
             await ctx.send(f"Successfully subscribed to {streamer}")
             write_to_json(subs)
             await sub_list(ctx)
 
+
 @client.command()
-async def remove_streamer(ctx,streamer):
-    subs=read_json()
-    user_id=ctx.author.id
+async def remove_streamer(ctx, streamer):
+    subs = read_json()
+    user_id = ctx.author.id
     if streamer in subs.keys():
         if user_id in subs[streamer]["subs"]:
             subs[streamer]["subs"].remove(user_id)
@@ -171,53 +188,57 @@ async def remove_streamer(ctx,streamer):
     else:
         await ctx.send("invalid streamer name, this command is case sensitive")
 
+
 @client.command()
 async def pi_temp(ctx):
-        temp=os.popen("vcgencmd measure_temp").read()
-        await ctx.send(f"pi {temp}")
+    temp = os.popen("vcgencmd measure_temp").read()
+    await ctx.send(f"pi {temp}")
+
 
 @client.command()
 async def settings(ctx):
-    settings=get_settings()
-    response=""
-    for setting,value in settings.items():
-        response+=f"{setting}: {value}\n"
+    settings = get_settings()
+    response = ""
+    for setting, value in settings.items():
+        response += f"{setting}: {value}\n"
     await ctx.send(response)
+
 
 @client.command()
 @commands.check(authorized)
-async def change_setting(ctx,setting,value):
-    already_added=False
-    settings=get_settings()
+async def change_setting(ctx, setting, value):
+    already_added = False
+    settings = get_settings()
     if setting in settings.keys():
-        old_value=settings[setting]
-        if setting=="refresh-time":
-            settings[setting]=int(value)
-            with open("settings.json","w") as f:
-                json.dump(settings,f,indent=4)
-            already_added=True
+        old_value = settings[setting]
+        if setting == "refresh-time":
+            settings[setting] = int(value)
+            with open("settings.json", "w") as f:
+                json.dump(settings, f, indent=4)
+            already_added = True
             streamer_live_check.change_interval(minutes=float(value))
             streamer_live_check.restart()
-        if setting=="status":
-            await client.change_presence(status=discord.Status.online,activity=discord.Game(value))
-        if setting=="maxoffline-time":
-            settings[setting]=int(value)
-            already_added=True
+        if setting == "status":
+            await client.change_presence(status=discord.Status.online, activity=discord.Game(value))
+        if setting == "maxoffline-time":
+            settings[setting] = int(value)
+            already_added = True
         if not already_added:
-            settings[setting]=value
-        with open("settings.json","w") as f:
-            json.dump(settings,f,indent=4)
-        
+            settings[setting] = value
+        with open("settings.json", "w") as f:
+            json.dump(settings, f, indent=4)
+
         await ctx.send(f"{setting}: {old_value}-->{value}")
     else:
         await ctx.send(f"setting: \"{setting}\" not found")
 
+
 @client.command()
 async def next_refresh(ctx):
-    next_iter=int(streamer_live_check.next_iteration.timestamp())
-    now=int(datetime.now().timestamp())
+    next_iter = int(streamer_live_check.next_iteration.timestamp())
+    now = int(datetime.now().timestamp())
 
-    delta_time=next_iter-now
+    delta_time = next_iter-now
 
     await ctx.send(f"Next refresh is in {delta_time} seconds")
 
--- twitch_notifications.py
+++ twitch_notifications.py
@@ -3,168 +3,182 @@ import requests
 import os
 import time
 
-json_file_name="streamers.json"
+json_file_name = "streamers.json"
+
 
 def get_streamers_to_check():
-    subs=read_json()
-    streamers_to_check=[]
-    epoch=int(time.time())
+    subs = read_json()
+    streamers_to_check = []
+    epoch = int(time.time())
     for streamer in subs.keys():
-        if subs[streamer]["timeout_until"]<=epoch:
+        if subs[streamer]["timeout_until"] <= epoch:
             streamers_to_check.append(streamer)
     return streamers_to_check
+
 
 def get_header(expired=False):
 
     if os.path.exists("header.json") and not expired:
-        with open("header.json","r") as f:
-            header=json.load(f)
+        with open("header.json", "r") as f:
+            header = json.load(f)
             return header
     else:
-        from secrets import client_id,secret_id
-        payload={
-            "client_id":client_id,
-            "client_secret":secret_id,
-            "grant_type":"client_credentials"}
+        from secrets import client_id, secret_id
+        payload = {
+            "client_id": client_id,
+            "client_secret": secret_id,
+            "grant_type": "client_credentials"}
 
-        r=requests.post("https://id.twitch.tv/oauth2/token", data=payload)
+        r = requests.post("https://id.twitch.tv/oauth2/token", data=payload)
         print("got another token")
         if r.ok:
-            AuthData=json.loads(r.text)
+            AuthData = json.loads(r.text)
 
-            access_token=AuthData["access_token"]
+            access_token = AuthData["access_token"]
 
-            header={"client-id":client_id,"Authorization": f"Bearer {access_token}"}
+            header = {"client-id": client_id,
+                      "Authorization": f"Bearer {access_token}"}
 
-            with open("header.json","w") as f:
-                json.dump(header,f,indent=4)
+            with open("header.json", "w") as f:
+                json.dump(header, f, indent=4)
 
             return header
         else:
             print("problem getting token")
 
+
 def get_streams(channel_id_list):
 
-    payload={
-        "user_login":channel_id_list
+    payload = {
+        "user_login": channel_id_list
     }
 
-    header=get_header()
+    header = get_header()
 
-    r=requests.get("https://api.twitch.tv/helix/streams",params=payload,headers=header)
+    r = requests.get("https://api.twitch.tv/helix/streams",
+                     params=payload, headers=header)
     if r.ok:
-        data=json.loads(r.text)["data"]
-        return data  
-    else:  
+        data = json.loads(r.text)["data"]
+        return data
+    else:
         print("request error,getting new key")
-        header=get_header(expired=True)
-        data=get_streams(channel_id_list)
+        header = get_header(expired=True)
+        data = get_streams(channel_id_list)
         return data
     return data
 
+
 def read_json():
     if os.path.exists(json_file_name):
-        with open(json_file_name,"r") as f:
-            subs=json.load(f)
+        with open(json_file_name, "r") as f:
+            subs = json.load(f)
         return subs
     else:
         return {}
 
-def write_to_json(to_write):
-    new_subs={}
-    for key,item in to_write.items():
-        if len(item["subs"])>0:
-            new_subs[key]=item
 
-    with open(json_file_name,"w") as f:
-        json.dump(new_subs,f,indent=4)
+def write_to_json(to_write):
+    new_subs = {}
+    for key, item in to_write.items():
+        if len(item["subs"]) > 0:
+            new_subs[key] = item
+
+    with open(json_file_name, "w") as f:
+        json.dump(new_subs, f, indent=4)
+
 
 def get_correct_user_name(streamer):
-    payload={
-        "login":streamer
+    payload = {
+        "login": streamer
     }
 
-    header=get_header()
+    header = get_header()
 
-    r=requests.get("https://api.twitch.tv/helix/users",params=payload,headers=header)
+    r = requests.get("https://api.twitch.tv/helix/users",
+                     params=payload, headers=header)
 
     if r.ok:
-        data=json.loads(r.text)["data"]
-        if len(data)>0: 
-            display_name=data[0]["display_name"]
+        data = json.loads(r.text)["data"]
+        if len(data) > 0:
+            display_name = data[0]["display_name"]
             return display_name
         else:
             return "not a valid user name"
-       
+
     else:
-        if r.status_code==400:
+        if r.status_code == 400:
             return "not a valid user name"
-        elif r.status_code==401:
+        elif r.status_code == 401:
             print("request error,getting new key")
-            header=get_header(expired=True)
-            messages=get_correct_user_name(streamer)
+            header = get_header(expired=True)
+            messages = get_correct_user_name(streamer)
             return messages
 
+
 def get_subbed_list(user_id):
-    subs=read_json()
-    user_subbed_list=[]
+    subs = read_json()
+    user_subbed_list = []
     for streamer in subs.keys():
         if user_id in subs[streamer]["subs"]:
             user_subbed_list.append(streamer)
     return user_subbed_list
 
+
 def streamer_lists(online_streamer_data):
-    online_list=[]
+    online_list = []
     for data in online_streamer_data:
         online_list.append(data["user_name"])
-    subs=read_json()
-    all_streamers=list(subs.keys())
-    offline_list=[]
+    subs = read_json()
+    all_streamers = list(subs.keys())
+    offline_list = []
     for streamer in all_streamers:
         if streamer not in online_list:
             offline_list.append(streamer)
-    return online_list,offline_list
+    return online_list, offline_list
 
-def increment_offline_time(offline_list,inc_amount):
-    subs=read_json()
+
+def increment_offline_time(offline_list, inc_amount):
+    subs = read_json()
     for streamer in subs.keys():
         if streamer in offline_list:
-            subs[streamer]["offline-time"]+=inc_amount
+            subs[streamer]["offline-time"] += inc_amount
     write_to_json(subs)
 
-def check_offline_time(online_list,messages,maxofflinetime):
-    subs=read_json()
+
+def check_offline_time(online_list, messages, maxofflinetime):
+    subs = read_json()
 
     for streamer in online_list:
-        if subs[streamer]["offline-time"]<maxofflinetime:
+        if subs[streamer]["offline-time"] < maxofflinetime:
             del messages[streamer]
 
     for streamer in online_list:
-        subs[streamer]["offline-time"]=0
+        subs[streamer]["offline-time"] = 0
 
     write_to_json(subs)
 
     return messages
 
-def time_formatting(hours,mins):
-    if hours!=0:
-        if mins==0 and hours>1:
+
+def time_formatting(hours, mins):
+    if hours != 0:
+        if mins == 0 and hours > 1:
             return f"{hours} hours ago"
-        elif mins==0 and hours==1:
+        elif mins == 0 and hours == 1:
             return f"{hours} hour ago"
         else:
-            if hours==1:
-                if mins==1:
+            if hours == 1:
+                if mins == 1:
                     return f"{hours} hour and {mins} minute ago"
-                else: 
+                else:
                     return f"{hours} hour and {mins} minutes ago"
             else:
-                if mins==1:
+                if mins == 1:
                     return f"{hours} hours and {mins} minute ago"
                 else:
                     return f"{hours} hours and {mins} minutes ago"
     else:
-        if mins==1 or mins==0:
+        if mins == 1 or mins == 0:
             return f"{mins} minute ago"
         else:
             return f"{mins} minutes ago"
